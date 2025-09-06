a2304402f68d4b83d7015d5ca209749b954d5bf7
Mohan <mohanjadhav57029@gmail.com>
2025-09-06 16:09:39 +0530

    add new code Nested loop Example

--- Loop.cpp
+++ Loop.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+// if else statement
 int main()
 // {int sum =0;
 //     int n;
@@ -16,16 +17,30 @@ int main()
 // return 0;
 
 //}
+// loopss
+// {
+//     int n;
+//     cout<<"Enter a Number ";
+//     cin>>n;
+// int fact =1;
+//     for (int i=1; i<=n;i++)
+// {fact*=i;}
 
+// cout<<"Factorial is "<<fact;
+// cout<<endl;
+// return 0;
+// }
+
+// Nested Loop Example 
 {
-    int n;
-    cout<<"Enter a Number ";
-    cin>>n;
-int fact =1;
-    for (int i=1; i<=n;i++)
-{fact*=i;}
-
-cout<<"Factorial is "<<fact;
+    int n=3;
+for(int i=4;i<=n;i++)
+{char ch='A';
+    for (int i=3;i<=n;i++ )
+    { cout<<ch;
+        ch =ch+1;
+    }
+}
 cout<<endl;
 return 0;
 }
