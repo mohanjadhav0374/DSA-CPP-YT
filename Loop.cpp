2b574c8cf4eaa338c5fed2eda437201027d218b3
Mohan <mohanjadhav57029@gmail.com>
2025-09-06 19:03:22 +0530

    Nested Loop Code

--- Loop.cpp
+++ Loop.cpp
@@ -32,15 +32,117 @@ int main()
 // }
 
 // Nested Loop Example 
-{
-    int n=3;
-for(int i=4;i<=n;i++)
-{char ch='A';
-    for (int i=3;i<=n;i++ )
+// Square Pattern
+
+//    { int n=5;
+//     char ch='A';
+// for(int i=1;i<=n;i++)
+// {
+//     for (int i=1;i<=n;i++ )
+//     { cout<<ch;
+//         ch =ch+1;
+//     }
+//     cout<<endl;
+// }
+
+// return 0;
+// }
+
+
+
+
+// Triangle Pattern
+// {
+//     int n;
+//     cout<<"Enter a number";
+//     cin>>n;
+//     for(int i=0;i<n;i++)
+//     {
+//         for(int j=1;j<=i+1;j++)
+//        { cout<<j;}
+//        cout<<endl;
+//     }
+  
+//     return 0;}
+
+
+// REverse Triangle pattern
+//    {int n;
+//     cout<<"Enter a number";
+//     cin>>n;
+//     char ch='A';
+//     for(int i=0;i<n;i++)
+//     {
+//         for(int j=i+1;j>0;j--)
+//        { cout<<ch;
+//     ch++;}
+//        cout<<endl;
+//     }
+  
+//     return 0;
+// } 
+
+// alphabate
+// {int n;
+//     cout<<"Enter a number";
+//     cin>>n;
+//     char ch='A';
+//     for(int i=0;i<n;i++)
+//     {
+//         for(int j=i+1;j>0;j--)
+//        { cout<<ch;
+//     ch++;}
+//        cout<<endl;
+//     }
+  
+//     return 0;
+// }
+
+
+// Inverted Triangle Pattern
+// {int n;
+//     cout<<"Enter a number";
+//     cin>>n;
+//     char ch='A';
+//     for(int i=0;i<n;i++)
+//     {  
+//         // for spaces
+      
+//         for(int j=0;j<i;j++)
+//         cout<<" ";
+//         // for num
+//         for(int j=0;j<n-i;j++)
+//        { cout<<ch;
+//     ch++;}
+//        cout<<endl;
+//     }
+  
+//     return 0;
+// }
+
+
+// Pyramid Pattern
+{int n;
+    cout<<"Enter a number";
+    cin>>n;
+    char ch='A';
+    for(int i=0;i<n;i++)
+    {  
+        // for spaces
+      
+        for(int j=0;j<n-i-1;j++)
+       { cout<<" ";}
+        // for num 1
+        for(int j=1;j<=i+1;j++)
+       { cout<<ch;
+    ch++;}
+
+    // for num 2
+    for(int j=i;j>0;j--)
     { cout<<ch;
-        ch =ch+1;
+    ch++;}
+       cout<<endl;
     }
-}
-cout<<endl;
-return 0;
+  
+    return 0;
 }
